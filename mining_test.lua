42143233ae6719b2107b7648f055887206216f06
Eric Lacker <eric@lacker.us>
2021-06-25 01:01:21 -0400

    Working on program. Straight line, comes back before running out of fuel. Auto refuel from found fuel items, places important items in an Ender chest.

--- mining_test.lua
+++ mining_test.lua
@@ -1,25 +1,27 @@
 -- Mining program v1 --
-
 -- Eric Lacker --
-
---  Orientation (start location = 0,0,0)
-local currX = 0
-local currY = 0
-local currZ = 0
-local width = 15
 
 -- Variables --
 
-IgnoredItems = {"minecraft:stone", "minecraft:cobblestone", "minecraft:gravel",
+IgnoredItem = {"minecraft:stone", "minecraft:cobblestone", "minecraft:gravel",
 "minecraft:dirt", "minecraft:water", "minecraft:red_mushroom", "minecraft:sand",
 "minecraft:netherrack", "Botania:stone", "Botania:mushroom", "chisel:limestone",
 "chisel:andesite", "chisel:diorite", "chisel:granite", "chisel:marble",
 "BiomesOPlenty:flowers", "BiomesOPlenty:flowers2"}
 
+-- short list, so this is a concise way of checking. Info doesn't
+-- have to be burn time.
+Consumable = {}
+Consumable["minecraft:coal"] = 1600
+Consumable["druidcraft:fiery_glass"] = 2400
+
+local distanceTraveled
+local fuel = turtle.getFuelLevel()
+
 -- Functions --
 
-function DoKeep(item)
-    for name in IgnoredItems
+function KeepItem(name)
+    for item in IgnoredItem
     do
         if name == item then
             return false
@@ -27,4 +29,63 @@ function DoKeep(item)
     end
     return true
 end
+
+function DigForward()
+    -- check if Lava, if so grab and refuel --
+    if turtle.detect() then
+        turtle.dig()
+    end
+    turtle.forward()
+    if turtle.getItemCount(13) > 0 then
+        MakeSpace()
+    end
+    distanceTraveled = distanceTraveled + 1
+end
+
+function MakeSpace()
+    --- check if can place chest ---
+    turtle.digDown()
+    turtle.select(1)
+    turtle.placeDown()
+    for slot = 3, 16
+    do
+        if Consumable[turtle.getItemDetail(slot).name] then
+            turtle.refuel(turtle.getItemDetail(slot).count)
+            fuel = turtle.getFuelLevel()
+        end
+        if KeepItem(turtle.getItemDetail(slot).name) then
+            turtle.dropDown()
+        end
+    end
+    turtle.select(1)
+    turtle.digDown()
+end
+
+function GoHome()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    if turtle.detectUp() then
+        turtle.digUp()
+    end
+    turtle.up()
+    for i = 1, distanceTraveled
+    do
+        turtle.forward()
+    end
+    turtle.down()
+    print("Needs Refueling.")
+    turtle.turnLeft()
+    turtle.turnLeft()
+end
+
+function Run()
+    while fuel > distanceTraveled
+    do
+        DigForward()
+        fuel = turtle.getFuelLevel()
+    end
+    GoHome()
+end
+
+Run()
 
