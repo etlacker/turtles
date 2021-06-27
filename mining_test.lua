0615eab815bb295f839da91e927dd04f9cfa1054
Eric Lacker <eric@lacker.us>
2021-06-27 01:27:31 -0400

    Bug fixes for item garbage functions, variables for available slots.

--- mining_test.lua
+++ mining_test.lua
@@ -17,11 +17,13 @@ Consumable["druidcraft:fiery_glass"] = 2400
 
 local distanceTraveled = 0
 local fuel = turtle.getFuelLevel()
+local firstUsed = 2
+local lastUsed = 4
 
 -- Functions --
 
 function KeepItem(name)
-    for _, item in IgnoredItem
+    for _, item in ipairs(IgnoredItem)
     do
         if name == item then
             return false
@@ -36,10 +38,18 @@ function DigForward()
         turtle.dig()
     end
     turtle.forward()
-    if turtle.getItemCount(13) > 0 then
+    if turtle.getItemCount(4) > 0 then
         MakeSpace()
     end
     distanceTraveled = distanceTraveled + 1
+end
+
+function DumpTrash()
+    for slot = firstUsed, lastUsed
+    do
+        turtle.select(slot)
+        turtle.dropDown()
+    end
 end
 
 function MakeSpace()
@@ -47,18 +57,22 @@ function MakeSpace()
     turtle.digDown()
     turtle.select(1)
     turtle.placeDown()
-    for slot = 3, 16
+    for slot = firstUsed, lastUsed
     do
         if Consumable[turtle.getItemDetail(slot).name] then
+            turtle.select(slot)
             turtle.refuel(turtle.getItemDetail(slot).count)
             fuel = turtle.getFuelLevel()
         end
         if KeepItem(turtle.getItemDetail(slot).name) then
+            turtle.select(slot)
             turtle.dropDown()
         end
     end
     turtle.select(1)
     turtle.digDown()
+
+    DumpTrash()
 end
 
 function GoHome()
